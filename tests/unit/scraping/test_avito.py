98ff34c06e620ed64ce43e489c2b01e2e46a17f0
Nijadi Mohamed <mnijadi@insea.ac.ma>
2024-09-30 17:45:19 +0100

    fix: refactor testing to adapt to utilities changes

--- tests/unit/scraping/test_avito.py
+++ tests/unit/scraping/test_avito.py
@@ -1,16 +1,23 @@
+import os
+import sys
 from typing import Dict, List, Tuple
 
 import pytest
 
-from tests.unit import conftest
+from tests.unit.conftest import make_response
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
+
+import var_avito as va
 
 # Tests based on the announcements listing page
 
 
 @pytest.mark.vcr
-def test_is_announcement_valid(avito_spider, get_announcements_a):
-    announcements_a = get_announcements_a
-    for test in [*conftest.AVITO_ANNOUNCEMENTS, conftest.AVITO_ANNOUNCEMENT_IMMONEUF]:
+def test_is_announcement_valid(avito_spider):
+    response = make_response(va.AVITO_PAGE_1)
+    announcements_a = response.css("div.sc-1nre5ec-1 a")
+    for test in [*va.AVITO_ANNOUNCEMENTS, va.AVITO_ANNOUNCEMENT_IMMONEUF]:
         assert (
             avito_spider.is_announcement_valid(announcements_a[test["index"]])
             == test["is_valid"]
@@ -18,9 +25,10 @@ def test_is_announcement_valid(avito_spider, get_announcements_a):
 
 
 @pytest.mark.vcr
-def test_get_info_from_announcement_a(avito_spider, get_announcements_a):
-    announcements_a = get_announcements_a
-    for test in conftest.AVITO_ANNOUNCEMENTS:
+def test_get_info_from_announcement_a(avito_spider):
+    response = make_response(va.AVITO_PAGE_1)
+    announcements_a = response.css("div.sc-1nre5ec-1 a")
+    for test in va.AVITO_ANNOUNCEMENTS:
         assert (
             avito_spider.get_info_from_announcement_a(announcements_a[test["index"]])
             == test["info"]
@@ -31,16 +39,16 @@ def test_get_info_from_announcement_a(avito_spider, get_announcements_a):
 @pytest.mark.parametrize(
     ("url", "expected_url"),
     [
-        (conftest.AVITO_PAGE_1, conftest.AVITO_PAGE_2),
-        (conftest.AVITO_PAGE_2, conftest.AVITO_PAGE_3),
-        (conftest.AVITO_PAGE_500, conftest.AVITO_PAGE_501),
-        (conftest.AVITO_PAGE_1000, conftest.AVITO_PAGE_1001),
-        (conftest.AVITO_PAGE_BEFORE_LAST, conftest.AVITO_PAGE_LAST),
-        (conftest.AVITO_PAGE_LAST, None),
+        (va.AVITO_PAGE_1, va.AVITO_PAGE_2),
+        (va.AVITO_PAGE_2, va.AVITO_PAGE_3),
+        (va.AVITO_PAGE_500, va.AVITO_PAGE_501),
+        (va.AVITO_PAGE_1000, va.AVITO_PAGE_1001),
+        (va.AVITO_PAGE_BEFORE_LAST, va.AVITO_PAGE_LAST),
+        (va.AVITO_PAGE_LAST, None),
     ],
 )
 def test_get_next_page_url(avito_spider, url: str, expected_url: str):
-    response = conftest.make_response(url)
+    response = make_response(url)
     assert avito_spider.get_next_page_url(response) == expected_url
 
 
@@ -50,28 +58,28 @@ def test_get_next_page_url(avito_spider, url: str, expected_url: str):
 @pytest.mark.vcr
 @pytest.mark.parametrize(
     ("url", "expected"),
-    [(test["url"], test["header"]) for test in conftest.AVITO_ANNOUNCEMENTS],
+    [(test["url"], test["header"]) for test in va.AVITO_ANNOUNCEMENTS],
 )
 def test_get_header(avito_spider, url: str, expected: Tuple[str, str, str, str, str]):
-    response = conftest.make_response(url)
+    response = make_response(url)
     assert avito_spider.get_header(response) == expected
 
 
 @pytest.mark.vcr
 @pytest.mark.parametrize(
     ("url", "expected"),
-    [(test["url"], test["attributes"]) for test in conftest.AVITO_ANNOUNCEMENTS],
+    [(test["url"], test["attributes"]) for test in va.AVITO_ANNOUNCEMENTS],
 )
 def test_get_attributes(avito_spider, url: str, expected: Dict[str, str]):
-    response = conftest.make_response(url)
+    response = make_response(url)
     assert avito_spider.get_attributes(response) == expected
 
 
 @pytest.mark.vcr
 @pytest.mark.parametrize(
     ("url", "expected"),
-    [(test["url"], test["equipments"]) for test in conftest.AVITO_ANNOUNCEMENTS],
+    [(test["url"], test["equipments"]) for test in va.AVITO_ANNOUNCEMENTS],
 )
 def test_get_equipments(avito_spider, url: str, expected: List[str]):
-    response = conftest.make_response(url)
+    response = make_response(url)
     assert avito_spider.get_equipments(response) == expected
