9d5103b73e54d7e8a201f832e9c3a78c0efce8b2
Nijadi Mohamed <mnijadi@insea.ac.ma>
2024-09-30 17:44:40 +0100

    chore: separate fixtures from mock tests results

---
+++ tests/unit/scraping/var_yakeey.py
@@ -0,0 +1,13 @@
+"""Variables to be used in test_yakeey.py"""
+
+# Requests to the following URLs are cached in tests/scraping/cassettes
+# if these caches are deleted, links might not work as the information
+# is constantly changing.
+
+YAKEEY_PAGE_1 = "https://yakeey.com/fr-ma/achat/appartement/maroc?page=1"
+YAKEEY_PAGE_2 = "https://yakeey.com/fr-ma/achat/appartement/maroc?page=2"
+YAKEEY_PAGE_3 = "https://yakeey.com/fr-ma/achat/appartement/maroc?page=3"
+YAKEEY_PAGE_15 = "https://yakeey.com/fr-ma/achat/appartement/maroc?page=15"
+YAKEEY_PAGE_16 = "https://yakeey.com/fr-ma/achat/appartement/maroc?page=16"
+YAKEEY_PAGE_BEFORE_LAST = "https://yakeey.com/fr-ma/achat/appartement/maroc?page=32"
+YAKEEY_PAGE_LAST = "https://yakeey.com/fr-ma/achat/appartement/maroc?page=33"
